b12105d75ee4a7d004216694631d7b65d3ddf572
Tomas Bjerre <tomas.bjerre85@gmail.com>
2024-12-10 19:21:34 +0100

    work

--- skapaveckor.js
+++ skapaveckor.js
@@ -14,8 +14,8 @@ dayjs.updateLocale('en', {
   var weekOfYear = require("dayjs/plugin/weekOfYear");
   dayjs.extend(weekOfYear);
   
-let now = dayjs("2024-10-07")
-for (let i = 0; i < 52*10; i++) {
+let now = dayjs("2024-12-10")
+for (let i = 0; i < 52*5; i++) {
     let vecka = now.week()
     console.log(`Week number: ${vecka}`)
     let year = now.format('YYYY')
@@ -33,22 +33,24 @@ for (let i = 0; i < 52*10; i++) {
 
     \\textbf{\\Huge Vecka ${vecka}, ${now.format('YYYY')}-${now.format('MM')}-${now.format('DD')} till ${to.format('YYYY')}-${to.format('MM')}-${to.format('DD')}} \\
 
+    \\textbf{\\LARGE ANGE LÄGENHETSNUMMER} \\
+
     \\renewcommand{\\arraystretch}{4}
     \\begin{table}[ht!]
     \\normalsize
     \\begin{tabular}{lllp{5cm}p{5cm}}
-    \\textbf{\\Huge Pass och dag} & \\textbf{\\Huge Tvättstuga 1} & \\textbf{\\Huge Tvättstuga 2} \\\\ \\hline    
+    \\textbf{\\LARGE Pass och dag} & \\textbf{\\LARGE Tvättstuga 1} & \\textbf{\\LARGE Tvättstuga 2} & \\textbf{\\LARGE Mangelrum} \\\\ \\hline    
     `
 
     for (let d =0;d < 7;d++) {
       let day = now.format('D')
       console.log(`day: ${day}`)
-      let dagensNamn = now.format('dddd')
+      let dagensNamn = now.format('dddd D/M')
       console.log(`dagens namn: ${dagensNamn}`)
 
       latexDocument += `
-      \\multicolumn{1}{|l|}{\\Huge \\textbf{${dagensNamn} förmiddag}}   & \\multicolumn{1}{l|}{} & \\multicolumn{1}{l|}{} \\\\ \\hline
-      \\multicolumn{1}{|l|}{\\Huge \\textbf{${dagensNamn} eftermiddag}} & \\multicolumn{1}{l|}{} & \\multicolumn{1}{l|}{} \\\\ \\hline    
+      \\multicolumn{1}{|l|}{\\LARGE \\textbf{${dagensNamn} förmiddag}}   & \\multicolumn{1}{l|}{} & \\multicolumn{1}{l|}{} & \\multicolumn{1}{l|}{} \\\\ \\hline
+      \\multicolumn{1}{|l|}{\\LARGE \\textbf{${dagensNamn} eftermiddag}} & \\multicolumn{1}{l|}{} & \\multicolumn{1}{l|}{} & \\multicolumn{1}{l|}{} \\\\ \\hline    
       `
 
       now = now.add(1,"days")
